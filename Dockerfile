4cabb8e845327c57187cd443df2c2dceea144260
Javier Vega <vega@debian>
2022-02-10 10:50:13 +0100

    t1-t2

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ COPY bookmedik /var/www/html/
 RUN rm /var/www/html/index.html
 ADD script.sh /usr/local/bin/
 EXPOSE 80
-ENV DATABASE_USER bookmedik
-ENV DATABASE_PASSWORD password
-ENV DATABASE_HOST server_mariadb
-ENV DATABASE_NAME bookmedik
+ENV DATABASE_USER=bookmedik
+ENV DATABASE_PASSWORD=password
+ENV DATABASE_HOST=server_mariadb
+ENV DATABASE_NAME=bookmedik
 RUN chmod +x /usr/local/bin/script.sh
-CMD ["bash","script.sh"]
+CMD ["script.sh"]
 
--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 sed -i 's/$this->user="root";/$this->user="'${DATABASE_USER}'";/g' /var/www/html/core/controller/Database.php
 sed -i 's/$this->pass="";/$this->pass="'${DATABASE_PASSWORD}'";/g' /var/www/html/core/controller/Database.php
 sed -i 's/$this->host="localhost";/$this->host="'${DATABASE_HOST}'";/g' /var/www/html/core/controller/Database.php
-sed -i 's/$this->ddbb="";/$this->$this->ddbb="'${DATABASE_NAME}'";/g' /var/www/html/core/controller/Database.php
+sed -i 's/$this->ddbb="";/$this->ddbb="'${DATABASE_NAME}'";/g' /var/www/html/core/controller/Database.php
 sleep 10
 mysql -u $DATABASE_USER -p$DATABASE_PASSWORD -h $DATABASE_HOST $DATABASE_NAME < /var/www/html/schema.sql
 apache2ctl -D FOREGROUND
